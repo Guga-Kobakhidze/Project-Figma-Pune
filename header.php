53003e64355f0b414f122e1dde8d176becfc8d92
Freek1ll3R <gugakobakhidze18@gmail.com>
2023-07-31 01:09:04 +0400

    Add ne project From Figma

---
+++ header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pune</title>
+    <link rel="stylesheet" href="./assets/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;500;600;700;800&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
+</head>
+
+<body>
+    <img class="background_img" src="./assets/images/Background.jpg" alt="BG">
+    <header class="header">
+        <div class="header_container">
+            <div class="header_section">
+                <div class="header_logo">
+                    <h1 class="header_logo_title"><a href="./index.php">PUNE</a></h1>
+                </div>
+                <nav>
+                    <ul class="navigation">
+                        <li><a href="#">Information</a></li>
+                        <li><a href="#">About us</a></li>
+                        <li><a href="#">Culture</a></li>
+                    </ul>
+                </nav>
+                <div class="search_button">
+                    <i class='bx bx-search search_btn'></i>
+                    <h3>Search</h3>
+                </div>
+            </div>
+        </div>
+    </header>
---
+++ index.php
@@ -0,0 +1,96 @@
+<?php include_once "header.php" ?>
+
+<section class="home_section_1">
+    <div class="header_container">
+        <div class="main_section_content">
+            <div class="link_image">
+                <img src="./assets/images/line.png" alt="line">
+                <a class="content_link_2" href="#">Queen of the Deccan</a>
+            </div>
+            <h1 class="main_title">Discover <br><span>Rapchik Pune</span></h1>
+        </div>
+        <div class="scroll_button">
+            <a class="content_link" href="#">Scroll down</a>
+            <img src="./assets/images/downarrow.png" alt="downarrow">
+        </div>
+        <div class="social_networks">
+            <h3 class="rotate_title">Follow us</h3>
+            <a href="#"><img src="./assets/images/instagram.png" alt="instagram"></a>
+            <a href="#"><img src="./assets/images/twitter.png" alt="twitter"></a>
+            <a href="#"><img src="./assets/images/Facebook.png" alt="facebook"></a>
+        </div>
+    </div>
+</section>
+<section class="home_section_2">
+    <div class="container">
+        <div class="content_section_1">
+            <img src="./assets/images/FirstSectionImage.png" alt="FirstSectionIMG">
+            <div class="fort_section_content">
+                <div class="link_image">
+                    <img src="./assets/images/line.png" alt="line">
+                    <a class="content_link_2" href="#">KING OF FORT</a>
+                </div>
+                <h2 class="content_title">Rajgad Fort</h2>
+                <p class="content_paragraph">Rajgad is a hill fort in Pune. It was initially called Murumbdev and was
+                    later renamed to Rajgad by Shivaji Maharaj. It is located south-west to Pune City. The base village
+                    Gunjavane is around 60km from Pune. This fort was initially the capital of Shivaji Maharaj's Hindavi
+                    Swarajya. The capital was later shifted to Raigad. The area of the fort base is 24 square miles.</p>
+                <div class="link_image">
+                    <a class="content_link" href="#">read more</a>
+                    <img src="./assets/images/buttonright.png" alt="butonRight">
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="home_section_3">
+    <div class="container">
+        <div class="content_section_2">
+            <img src="./assets/images/SecondSectionImage.png" alt="SecondSectionIMG">
+            <div class="inner_content">
+                <h2 class="content_title">Shreemant Dagdusheth Halwai Ganpati Mandir</h2>
+                <p class="content_paragraph">The Dagadusheth Halwai Ganapati temple in Pune is dedicated to the Hindu
+                    god Ganesh. The temple is visited by over hundred thousand pilgrims every year. Devotees of the
+                    temple include celebrities and chief ministers of Maharashtra who visit during the annual ten-day
+                    Ganeshotsav festival.</p>
+                <div class="link_image">
+                    <a class="content_link" href="#">read more</a>
+                    <img src="./assets/images/buttonright.png" alt="butonRight">
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="home_section_4">
+    <div class="container">
+        <div class="content_section_1 content_section_3">
+            <img src="./assets/images/thirdSectionImage.png" alt="FirstSectionIMG">
+            <div class="fort_section_content">
+                <div class="link_image">
+                    <img src="./assets/images/line.png" alt="line">
+                    <a class="content_link_2" href="#">f.c. road pune</a>
+                </div>
+                <h2 class="content_title">Our Culture Here Is Very Friendly To People</h2>
+                <p class="content_paragraph">Pune is a sprawling city in the western Indian state of Maharashtra. It was
+                    once the base of the Peshwas (prime ministers) of the Maratha Empire, which lasted from 1674 to
+                    1818. </p>
+                <div class="link_image">
+                    <a class="content_link" href="#">read more</a>
+                    <img src="./assets/images/buttonright.png" alt="butonRight">
+                </div>
+            </div>
+
+        </div>
+    </div>
+</section>
+<footer class="footer">
+    <div class="container">
+        <div class="footer_section">
+
+        </div>
+    </div>
+</footer>
+
+</body>
+
+</html>
